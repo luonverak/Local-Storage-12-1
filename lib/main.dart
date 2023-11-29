5762bcade3a0f56f6c97611f2403c017e3a4a9fe
luonverak <112792376+luonverak@users.noreply.github.com>
2023-11-29 13:26:13 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,58 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  static const myKey = 'myKey';
-  Future setCounter() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(myKey, _counter += 1);
-    getCounter();
-  }
-
-  Future getCounter() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _counter = prefs.getInt(myKey) ?? 0;
-    });
-  }
-
-  @override
-  void initState() {
-    getCounter();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Local storage'),
-      ),
-      body: Center(
-        child: Text(
-          '$_counter',
-          style: const TextStyle(fontSize: 30),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          setCounter();
-        },
-        child: const Icon(Icons.add),
-      ),
+      home: HomeScreen(),
     );
   }
 }
